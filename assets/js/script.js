8d298af7502f664f78dffdc37aae4bd1aedd2d6e
mhans19 <mdhansen19@gmail.com>
2020-05-26 18:33:58 -0600

    custom fonts and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -260,4 +260,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, 5000);
